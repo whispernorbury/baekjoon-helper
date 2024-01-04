89374b1562c9fae391189cf0705c508807ca97d8
Sunwoo Jeong <121336359+whispernorbury@users.noreply.github.com>
2024-01-05 02:58:32 +0900

    Update lotto.py

--- 15637 Lotto/lotto.py
+++ 15637 Lotto/lotto.py
@@ -7,22 +7,24 @@ url = "https://dhlottery.co.kr/gameResult.do?method=byWin"
 def crawl(idx):
     payload = { "drwNo": f"{idx}", "drwNoList": f"{idx}" }
     response = requests.post(url, data=payload)
-    soup = BeautifulSoup(response.text, "html.parser")
-    numwin = soup.find("div", "win")
-    nums = numwin.findAll("span", "ball_645")
-    for i in nums:
-        if i.text in answer:
-            answer[i.text] += 1
-        else:
-            answer[i.text] = 1
-
-
+    if respnse.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        numwin = soup.find("div", "win")
+        nums = numwin.findAll("span", "ball_645")
+        for i in nums:
+            if i.text in answer:
+                answer[i.text] += 1
+            else:
+                answer[i.text] = 1
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        return
+            
 print("Crawling started from dhlotto...")
 for i in range(1, 701):
     crawl(i)
     if i % 35 == 0:
         print(f"{int(i / 7)}% completed.")
-
 
 print('all calculated clear!')
 print('#####################')
